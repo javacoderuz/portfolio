35f398659a4aa135ec2059338c83401e264cec51
Isroil Ibroximov <158206284+javacoderuz@users.noreply.github.com>
2024-02-18 16:56:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+// Add your JavaScript code here
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Website loaded!");
+    // Your future JavaScript code would go here
+});
